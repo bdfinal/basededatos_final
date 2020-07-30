f2d172e8c4dcf31f1b49daeb4fcea114fbb7723c
FERNANDOBH502 <CitrusGnome@gmail.com>
2020-07-30 18:48:47 -0500

    Update index.php
    actualizacion del index

--- modulos/usuarios/index.php
+++ modulos/usuarios/index.php
@@ -28,7 +28,7 @@ $session = $ejecutar->usuarioActivo();
   <div class="container mt-5">
   <div class="row">
   <div class="col-sm-12">
-  <a href="../../cerrarsesion.php" class="btn btn-danger float-right mb-5">Cerrar Sesion</a>
+  <a href="../../index.php" class="btn btn-danger float-right mb-5">Cerrar Sesion</a>
   <a href="formularios_usuarios.php" class="btn btn-primary float-left mb-5">Nuevo</a>
   </div>
   <div class="col-sm-12">
@@ -37,25 +37,22 @@ $session = $ejecutar->usuarioActivo();
     <thead>
     <tr>    
     <th> Nombre</th>
-    
     <th>Correo electronico</th>
-    <th>teléfono</th>
-    <th >Acciones</th>
+    <th>Roles</th>
+    <th> Acciones </th>
     </tr>
     </thead>
     <tbody>
         <?php
-    while ($mostrar=mysqli_fetch_array($usr)){ //array: nos trae un arreglo de datos por posiciones, //2: arreglo asociativo podemos ver los campos de los bd
+    while ($mostrar=mysqli_fetch_array($usr)){ 
         ?>
         <tr>
-        <th><?php echo$mostrar['nombre'];?></th>
+        <th><?php echo$mostrar['nombre_usr'];?></th>
         <th><?php echo$mostrar['correo_usr'];?></th>
-        <th><?php echo$mostrar['telefono'];?></th>
+        <th><?php echo$mostrar['roles_id_rol'];?></th>
         <td><a href="fedicion_usuario.php?id=<?php echo $mostrar['id_usuario']; ?>">Editar</a>
         <a href="eliminar_usuarios.php?id=<?php echo $mostrar['id_usuario']; ?>">Eliminar</a>
-    
     </td>
-       
         </tr>
         <?php
     }
