3f6280f9832c406796d460071d3533274e41a82d
FERNANDOBH502 <CitrusGnome@gmail.com>
2020-08-03 17:25:29 -0500

    Update formularios_usuarios.php
    formulario usuario

--- modulos/usuarios/formularios_usuarios.php
+++ modulos/usuarios/formularios_usuarios.php
@@ -1,8 +1,12 @@
 <?php
+ include_once("../../funciones/consultas.php");
  include_once("../../funciones/funciones.php");
  include_once("../../funciones/bd.php");
  use funciones\mysqlfunciones;
+ use consultas_sql\consultas;
+ $consulta = new consultas();
  $ejecutar = new mysqlfunciones();
+ $rol = $consulta->rol();
  $session = $ejecutar->usuarioActivo();
  
 ?>
@@ -11,7 +15,7 @@
 <html lang="en">
 <head>
    <?php include("../../includes/header.php")?>
-    <title>Document</title>
+    <title>Nuevo Usuario</title>
 </head>
 <body>
 
@@ -30,11 +34,19 @@
         <input type="text" name="correo" id="correo" class="form-control" placeholder="Ingresa un correo electronico" required/>
         </div>
         <div class="form-group">
-
         <label class="left full">Contraseña</label>
         <input type="password" name="contraseña" id="contraseña" class="form-control" required placeholder="Ingresa una contraseña" />
         </div>
-       
+        <div class="form-group">
+        <label class="left full">Rol</label>
+        <select name="id_rol" id="id_rol" class="form-control">
+        <option value = "0">Selecciona una opción</option>
+       <?php
+        while ($qry=mysqli_fetch_array($rol )){ ?>      
+           <option value = "<?php echo $qry["id_rol"];?>"><?php echo $qry["nombre_rol"]?></option>
+           <?php }?>
+        </select>
+        </div>
     <div class="form-group">
     <input type="submit" value="Registrar usuario" class="btn btn-success">
     </div>
