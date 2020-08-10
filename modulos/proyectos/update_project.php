49542481e43b1647ea725b740ec4eed5b064304c
FERNANDOBH502 <CitrusGnome@gmail.com>
2020-08-10 17:59:15 -0500

    actualciazmabicoion fer
    actualizacion fer

--- modulos/proyectos/update_project.php
+++ modulos/proyectos/update_project.php
@@ -15,7 +15,9 @@ WHERE id_proyecto='$id'";
 $ejecucion = $ejecutar->ejecutar($qry);
 
 
-
+$qry2="INSERT INTO logs(accion_log, descripcion_log, fuente_log, responsable_log)
+VALUES ('UPDATE',CONCAT('Se ha modificado un  registro en la tabla proyectos'), ' proyectos', ' $id_log')";
+$ejecucion = $ejecutar->ejecutar($qry2);
 
 header("Location: all_project.php")
 ?>
--- modulos/roles/eliminar_roles.php
+++ modulos/roles/eliminar_roles.php
@@ -8,10 +8,15 @@ $ejecutar = new mysqlfunciones();
 
 
 $log_id=$_SESSION["id"];
+$id_log=$_SESSION["id"];
 $log_name=$_SESSION["nombre"];
 
 $id = $_GET["id"];
 $qry= "DELETE FROM roles WHERE id_rol = $id";
 $ejecucion = $ejecutar->ejecutar($qry);
+
+$qry2="INSERT INTO logs(accion_log, descripcion_log, fuente_log, responsable_log)
+VALUES ('DELETE',CONCAT('Se ha eliminado un  registro en la tabla roles'), ' roles', ' $id_log')";
+$ejecucion = $ejecutar->ejecutar($qry2);
 header("Location: index.php")
 ?>
